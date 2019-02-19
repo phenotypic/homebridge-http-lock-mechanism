dd29d3968d5ecd13eb6f0f96ab66a82e1ee4521b
Tom Rodrigues <33377034+Tommrodrigues@users.noreply.github.com>
2019-02-19 17:51:42 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,125 @@
+var Service, Characteristic;
+var request = require("request");
+
+module.exports = function(homebridge){
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  homebridge.registerAccessory("homebridge-http-lock", "HTTPLock", HTTPLock);
+};
+
+function HTTPLock(log, config) {
+  this.log = log;
+
+  this.name = config.name;
+  this.manufacturer = config.manufacturer || 'HTTP Manufacturer';
+  this.model = config.model || 'homebridge-http-garage';
+  this.serial = config.serial || 'HTTP Serial Number';
+
+  this.username = config.username || null;
+	this.password = config.password || null;
+  this.timeout = config.timeout || 5000;
+  this.http_method = config.http_method || 'GET';
+
+  this.openURL = config.openURL;
+  this.closeURL = config.closeURL;
+
+  this.autoLock = config.autoLock || false;
+  this.autoLockDelay = config.autoLockDelay || 10;
+
+  if(this.username != null && this.password != null){
+    this.auth = {
+      user : this.username,
+      pass : this.password
+    };
+  }
+
+  this.log(this.name);
+
+	this.service = new Service.HTTPLock(this.name);
+}
+
+HTTPLock.prototype = {
+
+	identify: function(callback) {
+		this.log("Identify requested!");
+		callback();
+	},
+
+  _httpRequest: function (url, body, method, callback) {
+      request({
+          url: url,
+          body: body,
+          method: this.http_method,
+          timeout: this.timeout,
+          rejectUnauthorized: false,
+          auth: this.auth
+      },
+          function (error, response, body) {
+              callback(error, response, body);
+          });
+  },
+
+  setLockTargetState: function(value, callback) {
+    this.log("[+] Setting LockTargetState to %s", value);
+    if (value == 1) { url = this.closeURL } else { url = this.openURL }
+    this._httpRequest(url, '', 'GET', function (error, response, responseBody) {
+        if (error) {
+          this.log("[!] Error setting LockTargetState: %s", error.message);
+					callback(error);
+        } else {
+          this.log("[+] Setting LockTargetState to %s", value);
+          if (value == 1) {
+            this.log("[*] Started closing");
+            this.simulateClose();
+          } else {
+            this.log("[*] Started opening");
+            this.simulateOpen();
+          }
+          callback();
+        }
+    }.bind(this));
+  },
+
+  simulateOpen: function() {
+    this.service.setCharacteristic(Characteristic.LockCurrentState, Characteristic.LockCurrentState.OPENING);
+    setTimeout(() => {
+      this.service.setCharacteristic(Characteristic.LockCurrentState, Characteristic.LockCurrentState.OPEN);
+      this.log("[*] Finished opening");
+    }, this.openTime * 1000);
+  },
+
+  simulateClose: function() {
+    this.service.setCharacteristic(Characteristic.LockCurrentState, Characteristic.LockCurrentState.CLOSING);
+    setTimeout(() => {
+      this.service.setCharacteristic(Characteristic.LockCurrentState, Characteristic.LockCurrentState.SECURED);
+      this.log("[*] Finished opening");
+    }, this.closeTime * 1000);
+  },
+
+	getName: function(callback) {
+		this.log("getName :", this.name);
+		callback(null, this.name);
+	},
+
+	getServices: function() {
+
+    this.service.setCharacteristic(Characteristic.LockCurrentState, Characteristic.LockCurrentState.SECURED);
+    this.service.setCharacteristic(Characteristic.LockTargetState, Characteristic.LockTargetState.SECURED);
+
+		this.informationService = new Service.AccessoryInformation();
+    this.informationService
+		  .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
+		  .setCharacteristic(Characteristic.Model, this.model)
+		  .setCharacteristic(Characteristic.SerialNumber, this.serial);
+
+		this.service
+			.getCharacteristic(Characteristic.Name)
+			.on('get', this.getName.bind(this));
+
+    this.service
+  		.getCharacteristic(Characteristic.LockTargetState)
+      .on('set', this.setLockTargetState.bind(this));
+
+		return [this.informationService, this.service];
+	}
+};
