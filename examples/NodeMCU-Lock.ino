5501b603623970b12bf14821b7a2ba7f616c6f47
phenotypic <33377034+phenotypic@users.noreply.github.com>
2020-05-12 16:50:40 +0200

    Update NodeMCU-Lock.ino

--- examples/NodeMCU-Lock.ino
+++ examples/NodeMCU-Lock.ino
@@ -1,5 +1,8 @@
 #include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServerSecure.h>
 #include <ESP8266mDNS.h>
+#include <ArduinoJson.h>
 
 // GitHub Page = https://github.com/Tommrodrigues/homebridge-http-lock-mechanism
 
@@ -10,19 +13,29 @@
 /////////////////// CHANGE THESE VALUES //////////////////////
 const char* ssid = "SSID"; // Name of your network
 const char* password = "PASSWORD"; // Password for your network
+const char* www_username = "admin";
+const char* www_password = "esp8266";
 const String relay = "HIGH"; // Relay type (`HIGH` or `LOW`)
 const char* mdns = "lock"; // mDNS name
-const char* onKey = "/SECRETKEY"; // e.g. "/NjWymLQzyd3PPp9N"
-const char* offKey = "/SECRETKEY"; // e.g. "/f3gZbxREJYFKRmz6"
 const int momentaryOn = 1000; // Delay time for the on state (ms)
 //////////////////////////////////////////////////////////////
+
+// The certificate is stored in PMEM
+static const uint8_t x509[] PROGMEM = {
+
+};
+
+// And so is the key.  These could also be in DRAM
+static const uint8_t rsakey[] PROGMEM = {
+
+};
 
 const int relayPin = 13;
 
 int relayOn, relayOff;
 int state = 1;
 
-WiFiServer server(80);
+ESP8266WebServerSecure server(443);
 
 void setup() {
   if (relay.equals("LOW")) {
@@ -55,9 +68,6 @@ void setup() {
   Serial.println();
   Serial.println("Connected successfully");
 
-  // Start the server
-  server.begin();
-
   // Print the IP address
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
@@ -66,55 +76,39 @@ void setup() {
     Serial.println("Error setting up MDNS responder!");
   }
   Serial.println("mDNS address: " + String(mdns) + ".local");
+
+  server.on("/setState", []() {
+    if (server.authenticate(www_username, www_password)) {
+      state = server.arg("value").toInt();
+      if (state) {
+        digitalWrite(relayPin, relayOn);
+        delay(momentaryOn);
+        digitalWrite(relayPin, relayOff);
+      } else {
+        digitalWrite(relayPin, relayOn);
+        delay(momentaryOn);
+        digitalWrite(relayPin, relayOff);
+      }
+      server.send(200);
+    }
+  });
+
+  server.on("/status", []() {
+    size_t capacity = JSON_OBJECT_SIZE(1);
+    DynamicJsonDocument doc(capacity);
+
+    doc["currentState"] = state;
+
+    String json;
+    serializeJson(doc, json);
+    server.send(200, "application/json", json);
+  });
+
+  // Start the server
+  server.begin();
 }
 
 void loop() {
-
+  server.handleClient();
   MDNS.update();
-
-  // Check if a client has connected
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
-  }
-
-  // Wait until the client sends some data
-  Serial.println("New client");
-  while(!client.available()){
-    delay(1);
-  }
-
-  // Read the first line of the request
-  String request = client.readStringUntil('\r');
-  Serial.println(request);
-  client.flush();
-
-  // Match the request
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/html");
-  client.println("");
-
-  if (request.indexOf(offKey) != -1)  {
-    digitalWrite(relayPin, relayOn);
-    delay(momentaryOn);
-    digitalWrite(relayPin, relayOff);
-    state = 1;
-  }
-
-  if (request.indexOf(onKey) != -1)  {
-    digitalWrite(relayPin, relayOn);
-    delay(momentaryOn);
-    digitalWrite(relayPin, relayOff);
-    state = 0;
-  }
-
-
-  if (request.indexOf("/status") != -1)  {
-    client.print(String(state));
-  }
-
-  delay(1);
-  Serial.println("Client disconnected");
-  Serial.println("");
-
 }
